25e052c119ef22b64573f81b76d3272b7d7e35b8
AAlbentosa <83396539+AAlbentosa@users.noreply.github.com>
2021-05-01 21:49:33 +0200

    Create B.java

---
+++ Regional 2019/B.-PoniendoLaMesa/B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PoniendoLaMesa {
+
+	public static void main(String[] args) {
+
+		Scanner Scan = new Scanner(System.in);
+        
+		while(true) {
+			 			
+			int copas=Scan.nextInt();
+			
+			if(copas==0) break;
+			
+            if(copas==1)System.out.println(1);
+            else System.out.println(copas-(copas%2));
+		}
+	}
+}
