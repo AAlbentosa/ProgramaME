60d559c945a47b138a1cf89a05b19c739729e2f9
AAlbentosa <83396539+AAlbentosa@users.noreply.github.com>
2021-06-10 19:10:23 +0200

    Create B.c

---
+++ Regional 2019/B.-PoniendoLaMesa/B.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void){
+    
+    int copas;
+    while(1){
+        scanf("%i", &copas);
+        if (copas==0)break;
+        
+        if(copas!=1){
+            printf ("%d\n",copas-copas%2);
+ 
+       }else{
+            printf ("1\n");
+        }
+    }
+    
+}
