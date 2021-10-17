c0c3e1eaf467bb01171536066c050b482e77dff3
AAlbentosa <83396539+AAlbentosa@users.noreply.github.com>
2021-10-17 17:00:01 +0200

    Change format

---
+++ Nacional 2021/E.-PeligroPorHielo/E.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void){
+        int n=1;
+        int number, count, alarm, x;
+        do{
+                count=0;
+                alarm=0;
+                scanf ("%d",&n);
+
+                for(x=0;x<n; x++){
+                        scanf( "%d", &number);
+                        if(number<=4 && alarm==0) {						
+			                    count++;
+			                    alarm=1;
+		                }else if(number>6 ) {
+				                alarm=0;
+		                }
+                }
+                printf("%d\n", count);
+        }while(n!=0);
+        return 0;
+}
