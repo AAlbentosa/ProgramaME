47a828b343f2467b75d6988ceb8f717306949ca0
AAlbentosa <83396539+AAlbentosa@users.noreply.github.com>
2021-10-17 18:00:04 +0200

    Update B.c

--- Regional 2019/B.-PoniendoLaMesa/B.c
+++ Regional 2019/B.-PoniendoLaMesa/B.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 int main(void){
-    
-    int copas;
-    while(1){
-        scanf("%i", &copas);
-        if (copas==0)break;
-        
-        if(copas!=1){
-            printf ("%d\n",copas-copas%2); 
-       }else{
-            printf ("1\n");
+        int copas;
+        while(1){
+                scanf("%i", &copas);
+                if (copas==0)break;
+                if(copas!=1)
+                    printf ("%d\n",copas-copas%2); 
+                else
+                    printf ("1\n");
         }
-    }
 }
